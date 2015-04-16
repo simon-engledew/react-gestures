78554602c3beda6b151c4283c320bafb4f76ce72
Christian Sullivan <cs@bodhi5.com>
2015-04-15 17:14:24 -0700

    babel 5.x fixes

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack')
+  , path = require('path');
+
+// Simple dev / production switch
+function _env(dev, production) {
+  return process.env.BUILD_ENV === 'production' ? production : dev;
+}
+
+function _path(p) {
+  return path.join(__dirname, p);
+}
+
+var compiler = module.exports = {
+  devtool: _env('eval', false),
+  entry: _path('src/index.js'),
+  output: {
+    path: _path('lib'),
+    filename: 'index.js',
+    library: 'react-gestures',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    loaders: [
+      { test: /\.(js|jsx)$/, loader: 'babel?stage=0&optional=runtime' },
+    ]
+  },
+  externals: {
+    react: 'React'
+  },
+  resolve: {
+    extensions: ['', '.react.js', '.js', '.jsx']
+  }
+};
